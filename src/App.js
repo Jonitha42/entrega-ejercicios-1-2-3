3accde7b564fc1c72ded30509b185277dcb35766
Pardo Jonathan <yonipardo4@gmail.com>
2022-09-15 22:45:31 -0300

    Ejercicio react 1-2-3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import ComponenteA from './components/ComponenteA';
 
 function App() {
+  const contactodePrueba = {
+    nombre: 'Jorge',
+    apellido: 'Fernandez',
+    email: 'jfernz@gmail.com',
+    conectado: false,
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ComponenteA contacto={contactodePrueba} />
       </header>
     </div>
   );
 }
-
 export default App;
